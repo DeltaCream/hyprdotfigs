1b639bbaae075ab889838aa873a163f7ae57a41d
DeltaCream <cseanwyatt@gmail.com>
2026-05-11 19:05:58 +0800

    feat: update arch dependency list

--- scripts/arch-dependencies.sh
+++ scripts/arch-dependencies.sh
@@ -5,5 +5,17 @@ sudo pacman -S gtk4 libadwaita gtk4-layer-shell
 # For hyprland
 paru -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender libxcursor pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git hyprwire-git xcb-util-errors hyprutils-git glaze hyprgraphics-git aquamarine-git re2 hyprland-qtutils-git muparser
 
+# For wayle
+sudo pacman -S --needed git gtk4 gtk4-layer-shell gtksourceview5 \
+  libpulse fftw libpipewire systemd-libs clang base-devel
+
 # For wayle (wayle-settings)
-paru -S gtksourceview5
+# paru -S gtksourceview5
+
+# Runtime daemons for the battery, bluetooth, network, power, and audio modules (skip any you don't need)
+sudo pacman -S --needed bluez bluez-utils networkmanager upower \
+  power-profiles-daemon pipewire wireplumber pipewire-pulse
+sudo systemctl enable --now bluetooth NetworkManager upower power-profiles-daemon
+
+# For wleave
+sudo pacman -S gtk4-layer-shell gtk4 librsvg libadwaita
